ec10981092817918bcf4cc3cf5498b112389a682
HeLi-Control <1939821708@qq.com>
2022-12-22 20:19:17 +0800

    Rechecked the  experiment 3.

--- OperatingSystem_3/sharedMemory.c
+++ OperatingSystem_3/sharedMemory.c
@@ -5,28 +5,27 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    int segment_id;
-    char *shared_memory;
-    const int size = 4096;
+    int segment_id = shmget(IPC_PRIVATE, 1024,
+                            S_IRUSR | S_IWUSR);
+    char *shared_memory = (char *)shmat(segment_id, NULL, 0);
 
-    segment_id = shmget(IPC_PRIVATE, size, S_IRUSR | S_IWUSR);
-    shared_memory = (char*)shmat(segment_id, NULL, 0);
     int pid = fork(); // Create a child process
-    if(pid < 0)
-    {
-        // fork failed
+    if (pid < 0)
+    { // fork failed
         printf("Frok failure!\n");
     }
-    else if(pid != 0)
+    else if (pid != 0)
     { // Father process
         // wait(NULL);
         printf("Parent Read: %s\n", shared_memory);
+        usleep(10);
+        // Detach and delete the shared memory.
         shmdt(shared_memory);
         shmctl(segment_id, IPC_RMID, NULL);
     }
-    else if(pid == 0)
+    else if (pid == 0)
     { // Child process
         printf("Child Write: Hi There!\n");
         sprintf(shared_memory, "Hi There!");
